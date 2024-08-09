7ea5cddaf60455754af60abbab56391f89b8f380
hujumsec <hujum.sec@proton.me>
2024-08-09 07:32:11 +0300

    Added helpful comments

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-SRC_FILES=$(find ./src/ -name '*.c')
+SRC_FILES=$(find ./src/ -name '*.c') # This grabs all the .c files inside the `src` directory
 
 mkdir build 2>/dev/null
 gcc -o build/offensecom $SRC_FILES -lncurses
-if [ $? -eq 0 ]; then
-	chmod +x build/offensecom
-	build/offensecom
+if [ $? -eq 0 ]; then      # Checks if the build is successful
+	chmod +x build/offensecom # Sets the output file as executable
+	build/offensecom          # Runs the output file
 fi
